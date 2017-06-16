450e39d225e3e9b21bef4ca9b300ea59e5f009c7
Paul <pvdstel@users.noreply.github.com>
2017-06-16 12:38:37 +0200

    Basic stuff and setup

---
+++ src/cgppm/Ppm/PpmParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgppm.Ppm
+{
+    public class PpmParser
+    {
+        /// <summary>
+        /// Reads the PPM image data from the stream.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <returns>A <see cref="RawPpmImage"/> containing the image data.</returns>
+        public RawPpmImage Read(Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+
+            string magicNumber;
+            int width, height;
+            short maxColorValue = byte.MaxValue;
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                magicNumber = sr.ReadLine();
+
+                string[] sizes = sr.ReadLine().Split(' ');
+                width = int.Parse(sizes[0]);
+                height = int.Parse(sizes[1]);
+
+                maxColorValue = short.Parse(sr.ReadLine());
+            }
+
+            return new RawPpmImage();
+        }
+
+        public RawPpmImage Read(string path)
+        {
+            if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException(nameof(path));
+
+            // IO exceptions will be thrown for us if it can't be found.
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                return Read(fs);
+            }
+        }
+    }
+}
---
+++ src/cgppm/Ppm/RawPpmImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgppm.Ppm
+{
+    /// <summary>
+    /// Represents a raw PPM image.
+    /// </summary>
+    public class RawPpmImage
+    {
+        /// <summary>
+        /// The magic number of the PPM image.
+        /// </summary>
+        public string MagicNumber { get; private set; } = string.Empty;
+
+        /// <summary>
+        /// The width of the image.
+        /// </summary>
+        public int Width { get; private set; } = 0;
+
+        /// <summary>
+        /// The height of the image.
+        /// </summary>
+        public int Height { get; private set; } = 0;
+
+        /// <summary>
+        /// The maximum color value of the image. The default value is the highest possible value for a byte.
+        /// </summary>
+        public short MaximumColorValue { get; private set; } = byte.MaxValue;
+    }
+}
