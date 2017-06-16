21a142063898131161635fbd8443d0ee4e63169a
Paul <pvdstel@users.noreply.github.com>
2017-06-16 12:48:26 +0200

    Better structuring

---
+++ src/cgppm/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace cgppm
+{
+    public class EntryPoint
+    {
+        private static IEnumerable<string> switches;
+        private static IEnumerable<string> files;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            switches = args.Where(s => s[0] == '-' || s[0] == '/').Select(s => s.Substring(1));
+            files = args.Where(s => File.Exists(s));
+        }
+    }
+}
