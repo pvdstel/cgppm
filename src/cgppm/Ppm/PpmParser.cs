43f333decb8bfdcfca39b4713f1e47fd03ae6369
Paul <pvdstel@users.noreply.github.com>
2017-06-16 22:56:19 +0200

    Factor out utilities

--- src/cgppm/Ppm/PpmParser.cs
+++ src/cgppm/Ppm/PpmParser.cs
@@ -9,9 +9,6 @@ namespace cgppm.Ppm
 {
     public class PpmParser
     {
-        private const byte LineFeed = 10;
-        private const byte CarriageReturn = 13;
-
         /// <summary>
         /// Reads the PBM image data from the stream.
         /// </summary>
@@ -30,8 +27,8 @@ namespace cgppm.Ppm
             // Detect the magic number
             while (magicNumber == null)
             {
-                string nextLine = GetStreamLine(stream);
-                if (!IsComment(nextLine))
+                string nextLine = stream.ReadSingleLine();
+                if (!nextLine.IsComment())
                 {
                     magicNumber = nextLine;
                     // Detect the maxium color value if specified
@@ -44,8 +41,8 @@ namespace cgppm.Ppm
             // Detect the image size
             while (!width.HasValue || !height.HasValue)
             {
-                string nextLine = GetStreamLine(stream);
-                if (!IsComment(nextLine))
+                string nextLine = stream.ReadSingleLine();
+                if (!nextLine.IsComment())
                 {
                     string[] sizes = nextLine.Split(' ');
                     width = int.Parse(sizes[0]);
@@ -55,8 +52,8 @@ namespace cgppm.Ppm
             // Detect the maximum color value
             while (!maxColorValue.HasValue)
             {
-                string nextLine = GetStreamLine(stream);
-                if (!IsComment(nextLine))
+                string nextLine = stream.ReadSingleLine();
+                if (!nextLine.IsComment())
                 {
                     maxColorValue = ushort.Parse(nextLine);
                 }
@@ -89,38 +86,6 @@ namespace cgppm.Ppm
             {
                 return Read(fs, detectMaximumColorValue);
             }
-        }
-
-        /// <summary>
-        /// Checks whether the given string is a comment.
-        /// </summary>
-        /// <param name="line">The string to check.</param>
-        /// <returns>A <see cref="bool"/> value indicating whether the given string is a comment.</returns>
-        private static bool IsComment(string line)
-        {
-            if (string.IsNullOrEmpty(line)) throw new ArgumentNullException(nameof(line));
-            return line[0] == '#';
-        }
-
-        /// <summary>
-        /// Reads a single line of text from stream.
-        /// </summary>
-        /// <param name="stream">The stream to read from.</param>
-        /// <returns>A <see cref="string"/> with the line that was read.</returns>
-        private static string GetStreamLine(Stream stream)
-        {
-            if (stream == null) throw new ArgumentNullException(nameof(stream));
-            if (!stream.CanRead) throw new InvalidOperationException("The stream cannot be read.");
-            if (stream.Length == 0) return string.Empty;
-
-            StringBuilder sb = new StringBuilder();
-            int nextByte = stream.ReadByte();
-            while (nextByte > 0 && nextByte != LineFeed && nextByte != CarriageReturn)
-            {
-                sb.Append((char)nextByte);
-                nextByte = stream.ReadByte();
-            }
-            return sb.ToString();
         }
     }
 }
---
+++ src/cgppm/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cgppm
+{
+    public static class Utilities
+    {
+        private const byte LineFeed = 10;
+        private const byte CarriageReturn = 13;
+
+        /// <summary>
+        /// Checks whether the given string is a comment.
+        /// </summary>
+        /// <param name="line">The string to check.</param>
+        /// <returns>A <see cref="bool"/> value indicating whether the given string is a comment.</returns>
+        public static bool IsComment(this string line)
+        {
+            if (string.IsNullOrEmpty(line)) throw new ArgumentNullException(nameof(line));
+            return line[0] == '#';
+        }
+
+        /// <summary>
+        /// Reads a single line of text from stream.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <returns>A <see cref="string"/> with the line that was read.</returns>
+        public static string ReadSingleLine(this Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanRead) throw new InvalidOperationException("The stream cannot be read.");
+            if (stream.Length == 0) return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            int nextByte = stream.ReadByte();
+            while (nextByte > 0 && nextByte != LineFeed && nextByte != CarriageReturn)
+            {
+                sb.Append((char)nextByte);
+                nextByte = stream.ReadByte();
+            }
+            return sb.ToString();
+        }
+    }
+}
