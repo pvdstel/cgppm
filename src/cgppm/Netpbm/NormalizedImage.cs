930554ca161064d795ee6e22dd2e0da525075829
Paul <pvdstel@users.noreply.github.com>
2017-06-17 01:31:16 +0200

    Add multiplication methods

--- src/cgppm/Netpbm/NormalizedImage.cs
+++ src/cgppm/Netpbm/NormalizedImage.cs
@@ -58,6 +58,66 @@ namespace cgppm.Netpbm
         }
 
         /// <summary>
+        /// Gets the normalized data as a byte array, multiplied by the specified number.
+        /// </summary>
+        /// <param name="multiplier">The number to multiply all normalized values with.</param>
+        /// <returns>A <see cref="byte[]"/> with multiplied normalized data.</returns>
+        public byte[] GetAsByteArray(byte multiplier)
+        {
+            byte[] result = new byte[ImageData.Length];
+            for (int i = 0; i < ImageData.Length; i++)
+            {
+                result[i] = (byte)(ImageData[i] * multiplier);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the normalized data as a 16 bit integer array, multiplied by the specified number.
+        /// </summary>
+        /// <param name="multiplier">The number to multiply all normalized values with.</param>
+        /// <returns>A <see cref="short[]"/> with multiplied normalized data.</returns>
+        public short[] GetAsInt16Array(short multiplier)
+        {
+            short[] result = new short[ImageData.Length];
+            for (int i = 0; i < ImageData.Length; i++)
+            {
+                result[i] = (short)(ImageData[i] * multiplier);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the normalized data as a 32 bit integer array, multiplied by the specified number.
+        /// </summary>
+        /// <param name="multiplier">The number to multiply all normalized values with.</param>
+        /// <returns>A <see cref="int[]"/> with multiplied normalized data.</returns>
+        public int[] GetAsInt32Array(int multiplier)
+        {
+            int[] result = new int[ImageData.Length];
+            for (int i = 0; i < ImageData.Length; i++)
+            {
+                result[i] = (int)(ImageData[i] * multiplier);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the normalized data as a double precision floating point array, multiplied by the specified number.
+        /// </summary>
+        /// <param name="multiplier">The number to multiply all normalized values with.</param>
+        /// <returns>A <see cref="double[]"/> with multiplied normalized data.</returns>
+        public double[] GetAsDoubleArray(double multiplier)
+        {
+            double[] result = new double[ImageData.Length];
+            for (int i = 0; i < ImageData.Length; i++)
+            {
+                result[i] = ImageData[i] * multiplier;
+            }
+            return result;
+        }
+
+        /// <summary>
         /// Normalizes image data to values between 0 and 1.
         /// </summary>
         /// <param name="rawData">The raw image data.</param>
