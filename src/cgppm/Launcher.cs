2ecd783d72baa5e8752cebfedb96c12a54370e73
Paul <pvdstel@users.noreply.github.com>
2017-06-17 02:03:44 +0200

    Improve Launcher code

--- src/cgppm/Launcher.cs
+++ src/cgppm/Launcher.cs
@@ -8,20 +8,25 @@ namespace cgppm
 {
     public class Launcher
     {
-        private static IEnumerable<string> switches;
-        private static IEnumerable<string> files;
 
         [STAThread]
         public static void Main(string[] args)
         {
-            switches = args.Where(s => s[0] == '-' || s[0] == '/').Select(s => s.Substring(1));
-            files = args.Where(s => File.Exists(s));
+            List<string> switches = args.Where(s => s[0] == '-' || s[0] == '/').Select(s => s.Substring(1)).ToList();
+            List<string> files = args.Where(s => File.Exists(s)).ToList();
 
             Parser parser = new Parser();
-            IEnumerable<RawImage> rawImages = files.Select(f => parser.Read(f));
-            IEnumerable<NormalizedImage> normalizedImages = rawImages.Select(ri => new NormalizedImage(ri));
-            
-            Console.WriteLine(string.Format("Successfully parsed {0} files.", normalizedImages.Count()));
+            List<RawImage> rawImages = files.Select(f => parser.Read(f)).ToList();
+
+            ImageConverter ic = new ImageConverter();
+            foreach (RawImage image in rawImages)
+            {
+                ImageViewer iv = new ImageViewer();
+                iv.SetBitmapSource(ic.ConvertNetpbmTo8Bit(image));
+                iv.Show();
+            }
+
+            App.Main();
         }
     }
 }
